83e853064e5a471e51b350930099f409ec0cc3e7
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2023-11-17 11:01:19 +0100

    feat(MyReview) : error deploiement vercel

--- 2024-nuxt/components/MyReview.vue
+++ 2024-nuxt/components/MyReview.vue
@@ -41,7 +41,7 @@ onMounted(() => {
                     <MyIcon @click="nextItem" name="arrowright" bgcolor="primary" size="small" />
                 </div>
             </div>
-            <img src="fastfood.jpg" alt="image de nourriture de fastfood" class="testimonials__content--image">
+            <img src="../public/Fastfood.jpg" alt="image de nourriture de fastfood" class="testimonials__content--image">
         </div>
 
     </div>
